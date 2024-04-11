7e27a5244b5cac52daac9eed52740349c2b78ba3
Igor Szot <szottt@hotmail.com>
2024-04-11 15:20:55 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.44.0"
     }
+  }
+  backend "s3" {
+    bucket = "comunidadedevops-cfg"
+    key    = "cfg/tfstate/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
